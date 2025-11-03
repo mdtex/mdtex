034a240fcbac492889889a6f1c62a8cca16afee9
Agastya Govind <79813984+ARGH23@users.noreply.github.com>
2025-11-02 23:06:53 -0600

    Update README (#13)

--- mdtexcli/src/cli/commands.rs
+++ mdtexcli/src/cli/commands.rs
@@ -6,31 +6,64 @@ use argh::FromArgs;
 pub(crate) struct CompileSubCommand {
     /// recursively compile files in a given folder
     #[argh(switch, short = 'r')]
-    recursive: bool,
+    pub(crate) recursive: bool,
 
-    // todo for html and pdf - make sure both are not checked at the same time
     /// specify we want our output as html file(s)
     #[argh(switch)]
-    html: bool,
+    pub(crate) html: bool,
 
     /// specify we want our output as pdf file(s)
     #[argh(switch)]
-    pdf: bool,
+    pub(crate) pdf: bool,
 
     /// specify output to a specific file - must be either pdf or html file extension
-    // todo: parse output into valid file format - if can't, throw error
     #[argh(option, short = 'o')]
-    output: Option<String>,
+    pub(crate) output: Option<String>,
 
     /// write output files recursively into a folder
     #[argh(option, long="write-recursive", short = 'w')]
-    write_recursive: Option<String>,
+    pub(crate) write_recursive: Option<String>,
 
     // (maybe): parse input into file or folder
     /// input file/folder
     #[argh(positional)]
-    input: String,
+    pub(crate) input: String,
 }
+
+
+impl CompileSubCommand {
+    /// Validates logical consistency of flags and options
+    pub fn validate(&self) -> Result<(), String> {
+        // --- 1. Disallow both --html and --pdf ---
+        if self.html && self.pdf {
+            return Err(String::from("Cannot specify both --html and --pdf at the same time."));
+        }
+
+        // --- 2. Validate output extension if provided ---
+        if let Some(ref output) = self.output {
+            if !(output.ends_with(".html") || output.ends_with(".pdf")) {
+                return Err(String::from(
+                    "Output file must have either a .html or .pdf extension.",
+                ));
+            }
+
+            // Optional consistency check
+            if self.html && !output.ends_with(".html") {
+                return Err(String::from(
+                    "Output file extension (.pdf) does not match the --html flag.",
+                ));
+            }
+            if self.pdf && !output.ends_with(".pdf") {
+                return Err(String::from(
+                    "Output file extension (.html) does not match the --pdf flag.",
+                ));
+            }
+        }
+
+        Ok(())
+    }
+}
+
 
 #[derive(FromArgs, PartialEq, Debug)]
 /// Usage: mdtex watch <flags> <input>
@@ -39,7 +72,7 @@ pub(crate) struct WatchSubCommand {
     // (maybe): parse input into file or folder
     /// input file/folder
     #[argh(positional)]
-    input: String,
+    pub(crate) input: String,
 }
 
 #[derive(FromArgs, PartialEq, Debug)]
@@ -48,7 +81,7 @@ pub(crate) struct WatchSubCommand {
 pub(crate) struct InitSubCommand {
     // parse input into folder?
     #[argh(positional)]
-    project_name: String,
+    pub(crate) project_name: String,
 }
 
 #[derive(FromArgs, PartialEq, Debug)]
@@ -56,7 +89,7 @@ pub(crate) struct InitSubCommand {
 #[argh(subcommand, name = "add")]
 pub(crate) struct AddSubCommand {
     #[argh(positional)]
-    add: String,
+    pub(crate) add: String,
 }
 
 #[derive(FromArgs, PartialEq, Debug)]
@@ -64,5 +97,25 @@ pub(crate) struct AddSubCommand {
 #[argh(subcommand, name = "build")]
 pub(crate) struct BuildSubCommand {
     #[argh(positional)]
-    build: String,
+    pub(crate) build: String,
+}
+
+
+/// Unified enum for all possible mdtex subcommands
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand)]
+pub(crate) enum SubCommand {
+    Compile(CompileSubCommand),
+    Watch(WatchSubCommand),
+    Init(InitSubCommand),
+    Add(AddSubCommand),
+    Build(BuildSubCommand),
+}
+
+/// Top-level mdtex command
+#[derive(FromArgs, PartialEq, Debug)]
+/// A Markdown-to-TeX converter CLI
+pub(crate) struct MdtexCommand {
+    #[argh(subcommand)]
+    pub(crate) subcommand: SubCommand,
 }
